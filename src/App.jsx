3864ed612d0775096c0d97b8a77e332622570828
Ignacio <ignacio.bertolotto@consultoracima.cl>
2026-01-12 17:13:50 -0300

    Estructura final corregida con archivos en raiz

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'        // Esto busca index.css en la carpeta src
+import App from './App.jsx'  // Esto busca App.jsx en la carpeta src
