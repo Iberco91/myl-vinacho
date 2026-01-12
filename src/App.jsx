6af43d351f446b75594e061119c7803e383d654f
Ignacio <ignacio.bertolotto@consultoracima.cl>
2026-01-12 17:18:28 -0300

    Borrado import de css en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'        // Esto busca index.css en la carpeta src
 import App from './App.jsx'  // Esto busca App.jsx en la carpeta src
