890bcd286439a03c46d5815c0040720ef8ba24a4
Ignacio <ignacio.bertolotto@consultoracima.cl>
2026-01-23 15:40:23 -0300

    Se agregaron nuevas aclaraciones hasta el id: 33

--- src/components/Aclaraciones.jsx
+++ src/components/Aclaraciones.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 
-//SEGUIR CON EL FAQ, ULTIMA AGREGADA: 7/143
+//SEGUIR CON EL FAQ, ULTIMA AGREGADA: 30/143
 export default function Aclaraciones () {
     // 1. Estado para manejar qué pregunta está abierta
     const [abierta, setAbierta] = useState(null);
@@ -79,6 +79,178 @@ export default function Aclaraciones () {
         pregunta: "Respecto a la carta Saladino, ¿Si yo genero los dos Oros en Fase de Vigilia, los puedo usar en Guerra de talismanes?",
         respuesta: "Todo lo que no especifique hasta cuando es la generación de Oros, es hasta la Fase Final."
         },
+        {
+        id: 12,
+        pregunta: "¿Como se resuelve Traficante de esclavos? (FAQ)",
+        respuesta: [
+                "Se agregó una errata a la carta, por lo tanto la resolucion de la carta es: En cuanto tu oponente reagrupe sus oros, puedes jugar este Talismán, para destruir ",
+                 "uno de tus Aliados y mover todos los Oros oponentes desde la reserva a su zona de Oro pagado. Luego tu oponente, podrá jugar su respectivo Oro desde la ",
+                 "mano y seguir adelante con el turno.",
+            ]
+        },
+        {
+        id: 13,
+        pregunta: "¿Qué pasa con la carta con habilidad “Solo puedes controlar una copia de esta carta cuando es jugada por habilidad de Afrodita, Idmon el Adivino o Sir Bedivere? (FAQ)",
+        respuesta: [
+                "Para los efectos de Afrodita, Idmon el Adivino y Sir Bedivere la carta no podrá ser jugada, ya que existe una carta ",
+                 "con la habilidad 'Sólo puedes controlar una copia de esta carta'. Por lo tanto quedará en su Zona de Origen, con la excepción de existir un efecto de reemplazo ",
+                 "como con Afrodita (barajando el Aliado) e Idmon el Adivino (enviando la carta al Cementerio).",
+
+            ]
+        },
+        {
+        id: 14,
+        pregunta: "¿Cuantos Aliados debe barajar Vouivre al entrar en juego? (FAQ)",
+        respuesta: [
+            "Se agrego una errata: 'Cuando Vouivre entra en juego, si tu oponente controla dos o más Aliados, puedes barajar un Aliado que controles, para barajar dos Aliados oponentes. ",
+             "En tu Fase de Vigilia una vez por turno puedes buscar un Aliado de raza Dragón en tu Mazo Castillo y ponerlo en tu mano. Carta Única'. Con esto se aclara que el efecto ",
+              "de barajar siempre deben ser dos Aliados oponentes."
+            ]
+        },
+        {
+        id: 15,
+        pregunta: "¿Debo mostrar la carta que barajo con Sir Erec al declararlo Atacante? (FAQ)",
+        respuesta: [
+                "En caso de resolver barajar 2 Aliados de raza Caballero, si se deben mostrar las cartas, para comprobar su raza y tipo de carta. ",
+                "En caso de resolver barajar una sola carta, al no tener condición para barajar, no es necesario mostrarla. ",
+            ]
+        },
+        {
+        id: 17,
+        pregunta: "¿Lanza divina puede hacer daño en guerra de talismanes del turno oponente? (FAQ)",
+        respuesta: [
+                "Así como muchas otras cartas que no digan “En tu Fase de Vigilia”. y si dicen Una vez por turno, la habilidad podrás activarla tanto en tu Fase de Vigilia, ",
+                "o en Guerra de Talismanes.",
+                " Esto nos da la posibilidad de que en el turno oponente, en Guerra de Talismanes, podré activar la habilidad de Lanza Divina para que tu oponente bote dos ",
+                "cartas de su Mazo Castillo. ",
+            ]
+        },
+        {
+        id: 18,
+        pregunta: "Si Orium no ataca el turno que entra en juego y ataca en otro turno. ¿Se destruye en Fase Final? (FAQ)",
+        respuesta: "Orium posee dos habilidades independientes entre si. Por lo tanto, el turno que sea declarado atacante debe ser destruido en la Fase Final."
+        },
+        {
+        id: 19,
+        pregunta: "¿Tormenta de Dragón, si me anulan el Talismán, debo desterrar el Aliado? (FAQ)",
+        respuesta: [
+                "Tormenta de Dragón debe resolverse para Desterrar el Aliado. Lo que si, al momento de ser jugado se debe declarar los objetivos y deben ser válidos para jugar la carta. ",
+                "Esto quiere decir que si el Talismán es anulado, no deberías desterrar un Aliado. ",
+                "Una vez jugado y que se resuelva (no haya sido anulado), destierras el Aliado que hiciste objetivo y buscarás el Aliado de coste 1 oro más para jugarlo sin pagar su coste."
+            ]
+        },
+        {
+        id: 20,
+        pregunta: "Con Merlín, ¿El Talismán jugado si es anulado se Destierra? (FAQ)",
+        respuesta: "Si, ya que el Talismán está siendo jugado de todas formas, por lo tanto aunque se Anule deberá ir al Destierro."
+        },
+        {
+        id: 21,
+        pregunta: "¿Goblin cambió de habilidad. ¿Cuál habilidad debo considerar válida ahora? (FAQ)",
+        respuesta: [
+                "Desde el lanzamiento de la Reposición de Relatos de Espada Sagrada, Ars Mágica, Goblin pasa a tener una nueva habilidad. La cual está descrita en su nueva caja de habilidad.",
+                " La habilidad previa queda descontinuada, y para efectos del juego debe usarse el nuevo en torneos oficiales."
+            ]
+        },
+        {
+        id: 22,
+        pregunta: "Menw, ¿Puedo elegirlo a él para que gane 1 de Fuerza e Imbloqueable? (FAQ)",
+        respuesta: [
+                "La regla de Auto-Referencia indica que la Fuente del efecto nunca puede ser Objetivo del mismo efecto.",
+                " Por lo tanto Menw no puede ser afectado por su mismo efecto."
+            ]
+        },
+        {
+        id: 23,
+        pregunta: "Círculo Eterno, hace que las cartas sean únicas. ¿Entonces, se puede jugar otra copia? (FAQ)",
+        respuesta: [
+                "La redacción de El Círculo Eterno hace que las cartas ganen la habilidad de Carta Única.",
+                " Como se sabe, esta es una habilidad de construcción de mazo, por lo tanto se podría jugar una copia sin problemas.",
+                " Lo que sí se debe respetar, es que al momento de entrar en juego El Círculo Eterno, se debe dejar una copia de cada carta en juego (que pueda ser Destruida)."
+            ]
+        },
+        {
+        id: 24,
+        pregunta: "¿La carta que destruya a Wyvern debe ser desterrada? (FAQ)",
+        respuesta: `Cualquier carta que destruya a Wyvern será Desterrada. 
+
+        Esta puede ser por ejemplo:
+        • Bola de Fuego. Una vez resuelta.
+        • Fergus al activar su efecto.
+        • Aliado bloqueador en la fase de Asignación de daño.`
+        },
+        {
+        id: 25,
+        pregunta: "Colmillo negro y modificaciones de fuerza posteriores (FAQ)",
+        respuesta: [
+                "En este caso particular, hay que tener en cuenta que tipo de habilidad tiene Colmillo Negro, esta posee una Habilidad Continua y por definicion esta se ",
+                "antepondra siempre a cualquier modificador de fuerza posterior a que dicha Arma sea portada en un Aliado en Juego.",
+                " Consideración: Jugar un Colmillo Negro sobre una Macha, no tendrá ningún efecto, ya que Macha no permite los modificadores de fuerza."
+            ]
+        },
+        {
+        id: 26,
+        pregunta: "Si tengo en juego la Corona de Arturo, ¿Mercaderes será destruido en la Fase Final? (FAQ)",
+        respuesta: [
+                "Si, ya que el efecto de considerarse Aliado de Mercaderes dura hasta el comienzo de la Fase Final, y en el momento que Mercaderes debe destruirse por su ",
+                "propia habilidad, estos ya dejaron de ser Aliados, por lo cual en ese momento la habilidad de Corona de Arturo no protegería a Mercaderes ya que no hay ",
+                "2 Aliados en juego. De otro modo, si en la Fase Final el jugador que controla Corona de Arturo llegase a tener dos o más Aliados en juego, Mercaderes ",
+                "se mantendrá en juego.",
+            ]
+        },
+        {
+        id: 27,
+        pregunta: "Si pago el coste de Activación de una carta que destruya una de mis cartas. ¿Puedo jugar Santo Grial y subirla a mi mano, y además resolver el efecto? (FAQ)",
+        respuesta: [
+                "Esta jugada no sería válida, ya que para resolver el efecto la carta debe ser Destruída. Un ejemplo es Activar Morgana, Destruir un Aliado. Se puede jugar el ",
+                "Santo Grial para subir dicho Aliado a la mano, pero no se resolverá el efecto de Morgana para desterrar un Aliado en juego. Si destruyo a Kernuac el Cazador y ",
+                " juego Santo Grial, el Kernuac se irá a la mano pero no podrán desterrarse las dos cartas del Mazo Castillo oponente.",
+            ]
+        },
+        {
+        id: 28,
+        pregunta: "Si un Aliado porta un Arma y es Destruido, si juego Santo Grial haciendo objetivo el Aliado, ¿Puedo subirme el Arma a la mano también? (FAQ)",
+        respuesta: "Si, ya que el Arma sigue al Aliado, por lo tanto ambas terminarán en tu Mano una vez resuelto el efecto de Santo Grial. ",
+        },
+        {
+        id: 29,
+        pregunta: "Si copio con Nimue a Bruja Anis, ¿Puedo robar en la Fase Final? (FAQ)",
+        respuesta: "No, ya que el efecto de Nimue cesa al inicio de la Fase Final. Por lo tanto ya no será Bruja Anis cuando sea el momento de poder robar una carta.",
+        },
+        {
+        id: 30,
+        pregunta: "¿Se puede anular un Aliado con inmunidad a talismanes? (no puede ser afectado por talismanes)? (FAQ)",
+        respuesta: "Si, esta es una jugada válida, ya que por regla general la inmunidad a Talismanes se aplica cuando el Aliado está en juego.",
+        },
+        {
+        id: 31,
+        pregunta: "¿Qué pasa con Juramento Feérico buscando Armas y portando a Aliados que no pueden ser declarados atacantes? (FAQ)",
+        respuesta: [
+                "En este caso, el Arma que esté siendo jugada con Juramento Feérico a un Aliado que no esté habilitado para atacar, (por ejemplo Ptha, Osiris, Ay), no podrán ",
+                "ser declarados Atacantes.",
+            ]
+        },
+        {
+        id: 32,
+        pregunta: "¿Cuánta fuerza tiene Balduino con Duque Godofredo en juego? (FAQ)",
+        respuesta: [
+                "Balduino pierde 1 a la Fuerza cada vez que ataque, esta reducción de Fuerza se aplica permanentemente a su Fuerza impresa en la carta, por lo cual este ",
+                "puede recibir bonificadores de cartas como por Ejemplo: Duque Godofredo o Hacha de Batalla, los cuales no serán restados por su Habilidad.",
+
+            ]
+        },
+        {
+        id: 33,
+        pregunta: "¿Qué pasa si copio con Nimue a un Greenknight y mi oponente le quita la habilidad con Carnwennan? (FAQ)",
+        respuesta: [
+                "La habilidad de Nimue hace que se convierta en una copia exacta de la carta hasta la Fase Final. Por lo tanto cualquier habilidad y/o efecto resuelto ",
+                "sobre la carta, afectará como si fuese un Greenknight, por lo tanto si pierde su habilidad tendrá fuerza cero.",
+
+            ]
+        },
+
+
+
     ];
 
     // LÓGICA DE FILTRADO: 
@@ -152,7 +324,7 @@ export default function Aclaraciones () {
                                 </button>
                                 {abierta === item.id && (
                             <div className="caja-respuesta">
-                                <p>{item.respuesta}</p>
+                                <p className="texto-punteo">{item.respuesta}</p>
                             </div>
                             )}
                             </div>
